959897813447f4bd753dc3e4959683831c06072f
Brett C. Dudo <brett.dudo@gmail.com>
2016-05-04 23:07:43 -0700

    change host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,7 +79,7 @@ Rails.application.configure do
   Aws::Rails.add_action_mailer_delivery_method(:aws_ses, credentials: aws_credentials, region: ENV['AWS_REGION'])
   config.action_mailer.delivery_method = :aws_ses
 
-  config.action_mailer.default_url_options = { host: 'dudo.io' }
+  config.action_mailer.default_url_options = { host: 'financialflowchart.herokuapp.com' }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
